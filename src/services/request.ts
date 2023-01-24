670884456d310da1cc96c6559a8a0a910eeba203
Julien Fraichot <12797962+lemoustachiste@users.noreply.github.com>
2023-01-24 16:06:37 +0100

    fix(Cache): Revert "[#264] disable cache by default but allow caching"
    some issues have appeared as servers can reject some requests if disallowed headers are set.
    As this is unpredictable, it seems frontend forcing cache control is not the right approach to solve #264

--- src/services/request.ts
+++ src/services/request.ts
@@ -6,7 +6,6 @@ export interface IRequestParameters {
   body?: any;
   forceHttp?: boolean;
   'bearer-token'?: string;
-  'allow-cache'?: boolean; // default false
 }
 
 export default async function request (obj: IRequestParameters): Promise<any> {
@@ -25,6 +24,10 @@ export default async function request (obj: IRequestParameters): Promise<any> {
     // server
     const XHR = typeof XMLHttpRequest === 'undefined' ? xhrPolyfill : XMLHttpRequest;
     const request: XMLHttpRequest = new XHR();
+
+    if (obj['bearer-token']) {
+      request.setRequestHeader('Authorization', `Bearer ${obj['bearer-token']}`);
+    }
 
     request.onload = () => {
       if (request.status >= 200 && request.status < 300) {
@@ -52,14 +55,6 @@ export default async function request (obj: IRequestParameters): Promise<any> {
     };
 
     request.open(obj.method || 'GET', url);
-
-    if (obj['bearer-token']) {
-      request.setRequestHeader('Authorization', `Bearer ${obj['bearer-token']}`);
-    }
-
-    if (!obj['allow-cache']) {
-      request.setRequestHeader('Cache-Control', 'no-cache, no-store, max-age=0');
-    }
 
     if (obj.body) {
       request.send(JSON.stringify(obj.body));
